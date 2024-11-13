9adfed754e10604ee3598ef62ab6558583d1fac4
MahlebMohamed <mohamed_mahleb@hotmail.com>
2024-11-13 15:50:24 +0100

    init project

---
+++ app/_components/NavBar.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function NavBar() {
+  const { isSignedIn, isLoaded } = useUser();
+
+  return (
+    <nav className="bg-base-300/30 px-5 py-4 md:px-[10%] md:py-6">
+      {isLoaded &&
+        (isSignedIn ? (
+          <>
+            <div className="flex items-center justify-between">
+              <h1 className="text-2xl font-bold">
+                e <span className="text-accent">track</span>
+              </h1>
+
+              <div className="hidden items-center justify-center gap-8 md:flex">
+                <Link className="btn" href={""}>
+                  Mes Budjets
+                </Link>
+                <Link className="btn" href={""}>
+                  Tableau de bord
+                </Link>
+                <Link className="btn" href={""}>
+                  Mes Transaction
+                </Link>
+              </div>
+
+              <UserButton />
+            </div>
+
+            <div className="mt-2 flex items-center justify-center gap-8 md:hidden">
+              <Link className="btn btn-sm" href={""}>
+                Mes Budjets
+              </Link>
+              <Link className="btn btn-sm" href={""}>
+                Tableau de bord
+              </Link>
+              <Link className="btn btn-sm" href={""}>
+                Mes Transaction
+              </Link>
+            </div>
+          </>
+        ) : (
+          <div className="flex items-center justify-between">
+            <div className="flex items-center text-2xl font-bold">
+              e <span className="text-accent">.Track</span>
+            </div>
+            <div className="mt-2 flex justify-center">
+              <Link href={"/sign-in"} className="btn btn-sm">
+                Se connecter
+              </Link>
+              <Link href={"/sign-up"} className="btn btn-accent btn-sm mx-4">
+                S&apos;inscrire
+              </Link>
+            </div>
+          </div>
+        ))}
+    </nav>
+  );
+}
